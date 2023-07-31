3aebf5522ccf57747e15f7619b60cc06682847c9
nkruger <nicolas.nkruger@gmail.com>
2023-07-31 13:49:27 -0300

    feat: :sparkles: layout

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <p>marketing layout</p>
+      {children}
+    </div>
+  );
+}
---
+++ src/app/(page)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      <p>page layout</p>
+      {children}
+    </div>
+  );
+}
