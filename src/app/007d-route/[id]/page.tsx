23eb543e7ef1e644f1494eb7f41fc3f71cfd1409
nkruger <nicolas.nkruger@gmail.com>
2023-07-31 14:05:16 -0300

    feat: :sparkles: get static param

--- src/app/007d-route/[id]/page.tsx
+++ src/app/007d-route/[id]/page.tsx
@@ -1,3 +1,13 @@
 export default function Page({ params }: { params: { id: string } }) {
   return <div>My Post: {params.id}</div>;
 }
+
+const getIds = async () => ["1", "2", "3"];
+
+export async function generateStaticParams() {
+  const ids = await getIds();
+
+  return ids.map((id) => ({
+    id: id,
+  }));
+}
